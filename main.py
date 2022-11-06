6a80b3f9d399174b3261aef9d0d2a213f147ad2b
Theo Fernandez <90938120+pondohoo@users.noreply.github.com>
2022-11-06 03:55:06 +0000

    Add finished product

--- main.py
+++ main.py
@@ -3,15 +3,10 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
+import time
 
-
-class Timer:
-  def __init__(self, hour, minute, second):
-    self.hour = 0
-    self.minute = 0
-    self.second = 0
-    
-
+status = 0
+seconds = 0
 
 intents = discord.Intents.default()
 intents.members = True
@@ -32,18 +27,53 @@ def secondsToTime(seconds):
     return "%d:%02d:%02d" % (hour, minutes, seconds)
   
 
-async def pomodoro(pomodorotime, breaktime, ctx):
-  pomodorotime = minutesToSeconds(pomodorotime)
-  notification = pomodorotime / 4 
-  timestring = secondsToTime(pomodorotime)
-  await ctx.send("```TIMELEFT:  %s```" %timestring)
-  pomodorotime -= notification
-  while pomodorotime > 0:
-    timestring = secondsToTime(pomodorotime)
+async def pomodoro(pomodorotime, breaktime, intervals, ctx):
+  for n in range(intervals):
+    if status == 0:
+      return
+    currentPomodoroTime = minutesToSeconds(pomodorotime)
+    notification = currentPomodoroTime / 4 
+    timestring = secondsToTime(currentPomodoroTime)
+    await ctx.send("```STARTING POMODORO %d```" %(n+1))
+    await asyncio.sleep(1)
+    await ctx.send("```TIME REMAINING:  %s```" %timestring)
+    currentPomodoroTime -= notification
+    while currentPomodoroTime > 0:
+      if status == 0:
+        return
+          
+      timestring = secondsToTime(currentPomodoroTime)
+      await asyncio.sleep(notification)
+      await ctx.send("```TIMELEFT:  %s```" %timestring)
+      currentPomodoroTime -= notification
+      
+    
     await asyncio.sleep(notification)
-    await ctx.send("```TIMELEFT:  %s```" %timestring)
-    pomodorotime -= notification
-  await ctx.send("```DONE  ```")
+    await ctx.send("```DONE```")
+    await asyncio.sleep(1)
+    await ctx.send("```STARTING BREAK```")
+    await asyncio.sleep(1)
+    
+    currentBreakTime = minutesToSeconds(breaktime)
+    breakNotification = currentBreakTime / 4
+    breaktimestring = secondsToTime(currentBreakTime)
+    await ctx.send("```TIME REMAINING:  %s```" %breaktimestring)
+    currentBreakTime -= breakNotification
+    
+    while currentBreakTime > 0:
+      if status == 0:
+        return
+      breaktimestring = secondsToTime(currentBreakTime)
+      await asyncio.sleep(breakNotification)
+      await ctx.send("```TIMELEFT:  %s```" %breaktimestring)
+      currentBreakTime -= breakNotification
+    await asyncio.sleep(breakNotification)
+    await ctx.send("```DONE```")
+    await asyncio.sleep(1)
+      
+    
+    
+  
 
 
 @bot.event
@@ -57,87 +87,76 @@ bot.remove_command('help')
 
 @bot.command()
 async def help(ctx):
-    title="Welcome to Pomodoro"
-    description="Available Commands:\n!pom start \"minutes for work\" \"minutes for break\" \n!pom pause \n!pom resume \n!pom cancel"
-    embedThis = discord.Embed(title=title, description=description)
-    await ctx.send(embed=embedThis)
+    # title="Welcome to Pomodoro"
+    # description="Available Commands:\n!pom start \"minutes for work\" \"minutes for break\" \" number of intervals\" \n!pom pause \n!pom resume \n!pom cancel"
+    # embedThis = discord.Embed(title=title, description=description)
+    # await ctx.send(embed=embedThis)
+  embed=discord.Embed(title="Welcome to PomoDiscordo Timer ✨", description="Get your money up, not your funny up.", color=0x46dde5)
+  embed.set_thumbnail(url="https://i.imgur.com/Ei0Uq0d.jpg")
+  embed.add_field(name="Get Started With", value="!pom default", inline=True) 
+  embed.add_field(name="Need to Stop?:", value="!pom cancel",inline=True)
+  embed.add_field(name="Custom Time:", value="!pom start<worktime> <breaktime> <intervals>", inline=False)
+  embed.set_footer(icon_url=ctx.author.display_avatar,text="Session begun by: {}".format(ctx.author.display_name))
+  await ctx.send(embed=embed)
 
 @bot.command()
-async def start(ctx, pomodorot: int, breaktime: int):
-   await pomodoro(pomodorot, breaktime, ctx)
+async def start(ctx, pomodorot: int, breaktime: int, intervals: int):
+  global status
+  if status != 1:
+    if pomodorot < 0 or pomodorot > 180:
+       await ctx.send("Please enter a pomodoro minutes value from 0 to 180")
     
+      
+    if breaktime < 0 or breaktime > 180:
+      await ctx.send("Please enter a break minutes value from 0 to 180")
+      
+    if intervals < 0 or intervals > 10:
+      await ctx.send("Please enter an interval numbers value from 0 to 10")
+    else: 
+      status = 1
+      await pomodoro(pomodorot, breaktime, intervals, ctx)
+      
 
   
+
+  else:
+    await ctx.send("Pomodoro in-progress\ntype \"!pom cancel\" to cancel it")
+    
+
+@bot.command()
+async def default(ctx):
+  global status
+  if status != 1:
+    
+    status = 1
+    await pomodoro(25, 5, 4, ctx)
+  else:
+    await ctx.send("Pomodoro in-progress\ntype \"!pom cancel\" to cancel it")
+
+  
+
+
+
 @bot.command()
 async def pause(ctx):
+    global status
+    status = False
     await ctx.send("Pomodoro paused")
 
 @bot.command()
 async def resume(ctx):
+    global resume
+    resume = True
     await ctx.send("Pomodoro resumed")
 
 @bot.command()
 async def cancel(ctx):
-    await ctx.send("Pomodoro cancelled")
-
-
-# @bot.command()
-# async def kennytest(ctx):
-#     await ctx.send("```css\n [w]```")
-
-
-
-# @bot.command()
-# async def add(ctx, left: int, right: int):
-#     """Adds two numbers together."""
-#     await ctx.send(left + right)
-
-
-# @bot.command()
-# async def roll(ctx, dice: str):
-#     """Rolls a dice in NdN format."""
-#     try:
-#         rolls, limit = map(int, dice.split('d'))
-#     except Exception:
-#         await ctx.send('Format has to be in NdN!')
-#         return
-
-#     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-#     await ctx.send(result)
-
-
-# @bot.command(description='For when you wanna settle the score some other way')
-# async def choose(ctx, *choices: str):
-#     """Chooses between multiple choices."""
-#     await ctx.send(random.choice(choices))
-
-
-# @bot.command()
-# async def repeat(ctx, times: int, content='repeating...'):
-#     """Repeats a message multiple times."""
-#     for i in range(times):
-#         await ctx.send(content)
-
-
-# @bot.command()
-# async def joined(ctx, member: discord.Member):
-#     """Says when a member joined."""
-#     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
-
-
-# @bot.group()
-# async def cool(ctx):
-#     """Says if a user is cool.
-#     In reality this just checks if a subcommand is being invoked.
-#     """
-#     if ctx.invoked_subcommand is None:
-#         await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
-
-
-# @cool.command(name='bot')
-# async def _bot(ctx):
-#     """Is the bot cool?"""
-#     await ctx.send('Yes, the bot is cool.') 
+    global status
+    if status == 0:
+      await ctx.send("No active Pomodoro")
+    else:
+      status = 0
+      await ctx.send("Pomodoro cancelled")
 
 my_secret = os.environ['token']
 bot.run(my_secret)
